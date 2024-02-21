0fa0ce05cb7af682cb19354b3a456245048d740c
Moreraz20 <felipemorera15@gmail.com>
2024-02-20 21:25:08 -0500

    Refactor: udistrital/sga_documentacion#67 Cambio de nombre a los importes

--- routers/router.go
+++ routers/router.go
@@ -8,7 +8,7 @@
 package routers
 
 import (
-	"github.com/udistrital/sga_mid_proyecto_curricular/controllers"
+	"github.com/udistrital/sga_proyecto_curricular_mid/controllers"
 
 	"github.com/astaxie/beego"
 )
