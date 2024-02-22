bb50a685cc7f9906fe03ad16784686285154c28a
Moreraz20 <felipemorera15@gmail.com>
2024-02-21 22:40:25 -0500

    Refactor: udistrital/sga_documentacion#67 Refactor Crear proyecto

--- services/crear_proyecto_academico_service.go
+++ services/crear_proyecto_academico_service.go
@@ -1,87 +1,95 @@
 package services
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
 
 	"github.com/astaxie/beego"
-	"github.com/udistrital/sga_proyecto_curricular_mid/helpers"
-	"github.com/udistrital/sga_proyecto_curricular_mid/models"
 	"github.com/udistrital/utils_oas/request"
+	"github.com/udistrital/utils_oas/requestresponse"
 	"github.com/udistrital/utils_oas/time_bogota"
 )
 
 // FUNCIONES QUE SE USAN EN PUT GET ONE POST COORDINADOR BY ID
 
-func asignarProyectoAcademico(alertas *[]interface{}, Proyecto_academico *map[string]interface{}, resultadoOikos map[string]interface{}, Proyecto_academicoPost *map[string]interface{}) {
-	*alertas = append(*alertas, *Proyecto_academico)
+func asignarProyectoAcademico( Proyecto_academico *map[string]interface{}, resultadoOikos map[string]interface{}, Proyecto_academicoPost *map[string]interface{}) {
 	idDependenciaProyecto := resultadoOikos["HijaId"].(map[string]interface{})["Id"]
 	(*Proyecto_academicoPost)["ProyectoAcademicoInstitucion"].(map[string]interface{})["DependenciaId"] = idDependenciaProyecto
 }
 
-func peticionOikos(resultadoOikos *map[string]interface{}, Proyecto_academico_oikosPost interface{}, alerta *models.Alert, alertas *[]interface{}, Proyecto_academico *map[string]interface{}, Proyecto_academicoPost *map[string]interface{}) bool {
+func peticionOikos(resultadoOikos *map[string]interface{}, Proyecto_academico_oikosPost interface{}, Proyecto_academico *map[string]interface{}, Proyecto_academicoPost *map[string]interface{}) bool {
 	errOikos := request.SendJson("http://"+beego.AppConfig.String("OikosService")+"/dependencia_padre/tr_dependencia_padre", "POST", resultadoOikos, Proyecto_academico_oikosPost)
 	if (*resultadoOikos)["Type"] == "error" || errOikos != nil || (*resultadoOikos)["Status"] == "404" || (*resultadoOikos)["Message"] != nil {
-		helpers.ManejoError(alerta, alertas, fmt.Sprintf("%v", resultadoOikos), errOikos)
+
 		return false
 	} else {
-		asignarProyectoAcademico(alertas, Proyecto_academico, *resultadoOikos, Proyecto_academicoPost)
+		asignarProyectoAcademico(Proyecto_academico, *resultadoOikos, Proyecto_academicoPost)
 		return true
 	}
 }
 
-func peticionProyecto(resultadoProyecto *map[string]interface{}, Proyecto_academicoPost map[string]interface{}, alerta *models.Alert, alertas *[]interface{}, Proyecto_academico map[string]interface{}) bool {
+func peticionProyecto(resultadoProyecto *map[string]interface{}, Proyecto_academicoPost map[string]interface{}, Proyecto_academico map[string]interface{}) bool {
 	errProyecto := request.SendJson("http://"+beego.AppConfig.String("ProyectoAcademicoService")+"/tr_proyecto_academico", "POST", resultadoProyecto, Proyecto_academicoPost)
 	if (*resultadoProyecto)["Type"] == "error" || errProyecto != nil || (*resultadoProyecto)["Status"] == "404" || (*resultadoProyecto)["Message"] != nil {
-		helpers.ManejoError(alerta, alertas, "", errProyecto)
+
 		return false
 	} else {
-		*alertas = append(*alertas, Proyecto_academico)
 		return true
 	}
 }
 
-func ManejoPeticionesProyecto(Proyecto_academico *map[string]interface{}, alerta *models.Alert, alertas *[]interface{}) bool {
-	Proyecto_academicoPost := make(map[string]interface{})
-	Proyecto_academicoPost = map[string]interface{}{
-		"ProyectoAcademicoInstitucion": (*Proyecto_academico)["ProyectoAcademicoInstitucion"],
-		"Enfasis":                      (*Proyecto_academico)["Enfasis"],
-		"Registro":                     (*Proyecto_academico)["Registro"],
-		"Titulaciones":                 (*Proyecto_academico)["Titulaciones"],
+func ManejoPeticionesProyecto(data []byte)(APIResponseDTO requestresponse.APIResponse) {
+	var Proyecto_academico *map[string]interface{}
+
+	if err := json.Unmarshal(data, &Proyecto_academico); err == nil {
+		Proyecto_academicoPost := make(map[string]interface{})
+		Proyecto_academicoPost = map[string]interface{}{
+			"ProyectoAcademicoInstitucion": (*Proyecto_academico)["ProyectoAcademicoInstitucion"],
+			"Enfasis":                      (*Proyecto_academico)["Enfasis"],
+			"Registro":                     (*Proyecto_academico)["Registro"],
+			"Titulaciones":                 (*Proyecto_academico)["Titulaciones"],
+		}
+	
+		Proyecto_academico_oikosPost := (*Proyecto_academico)["Oikos"]
+	
+		var resultadoOikos map[string]interface{}
+		var resultadoProyecto map[string]interface{}
+	
+		if !peticionOikos(&resultadoOikos, Proyecto_academico_oikosPost, Proyecto_academico, &Proyecto_academicoPost) {
+			APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil)
+			return APIResponseDTO
+		}
+	
+		if !peticionProyecto(&resultadoProyecto, Proyecto_academicoPost,*Proyecto_academico) {
+			APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil)
+			return APIResponseDTO
+		}
+		APIResponseDTO = requestresponse.APIResponseDTO(true, 200, nil)
+		return APIResponseDTO
+	}else {
+		APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil, err.Error())
+		return APIResponseDTO
 	}
-
-	Proyecto_academico_oikosPost := (*Proyecto_academico)["Oikos"]
-
-	var resultadoOikos map[string]interface{}
-	var resultadoProyecto map[string]interface{}
-
-	if !peticionOikos(&resultadoOikos, Proyecto_academico_oikosPost, alerta, alertas, Proyecto_academico, &Proyecto_academicoPost) {
-		return false
-	}
-
-	if !peticionProyecto(&resultadoProyecto, Proyecto_academicoPost, alerta, alertas, *Proyecto_academico) {
-		return false
-	}
-	return true
 }
 
 // FUNCIONES QUE SE USAN EN PUT GET ONE POST COORDINADOR BY ID
 
-func RegistrarCoordinador(alertas *[]interface{}, alerta *models.Alert, CoordinadorNuevo map[string]interface{}) bool {
+func RegistrarCoordinador( CoordinadorNuevo map[string]interface{}) bool {
 	var resultadoCoordinadorNuevo map[string]interface{}
 	CoordinadorNuevo["FechaFinalizacion"] = "0001-01-01T00:00:00-05:00"
 
 	errRegistro := request.SendJson("http://"+beego.AppConfig.String("ProyectoAcademicoService")+"/proyecto_academico_rol_tercero_dependencia", "POST", &resultadoCoordinadorNuevo, CoordinadorNuevo)
 	if resultadoCoordinadorNuevo["Type"] == "error" || errRegistro != nil || resultadoCoordinadorNuevo["Status"] == "404" || resultadoCoordinadorNuevo["Message"] != nil {
-		helpers.ManejoError(alerta, alertas, fmt.Sprintf("%v", resultadoCoordinadorNuevo))
+
 		return false
 	} else {
-		*alertas = append(*alertas, CoordinadorNuevo)
+
 		return true
 	}
 }
 
-func ManejoCoordinadorAntiguo(alertas *[]interface{}, alerta *models.Alert, CoordinadorAntiguos []map[string]interface{}) bool {
+func ManejoCoordinadorAntiguo(CoordinadorAntiguos []map[string]interface{}) bool {
 	for _, cordinadorFecha := range CoordinadorAntiguos {
 		if cordinadorFecha["Activo"] == true {
 			cordinadorFecha["Activo"] = false
@@ -92,7 +100,6 @@ func ManejoCoordinadorAntiguo(alertas *[]interface{}, alerta *models.Alert, Coor
 			var resultado map[string]interface{}
 			errcoordinadorcambiado := request.SendJson("http://"+beego.AppConfig.String("ProyectoAcademicoService")+"/proyecto_academico_rol_tercero_dependencia/"+strconv.FormatFloat(idcoordinador, 'f', -1, 64), "PUT", &resultado, &coordinador_cambiado)
 			if resultado["Type"] == "error" || errcoordinadorcambiado != nil || resultado["Status"] == "404" || resultado["Message"] != nil {
-				helpers.ManejoError(alerta, alertas, fmt.Sprintf("%v", resultado))
 				return false
 			}
 		} else {
@@ -101,4 +108,64 @@ func ManejoCoordinadorAntiguo(alertas *[]interface{}, alerta *models.Alert, Coor
 
 	}
 	return true
+}
+
+func CoordinaById(data []byte)(APIResponseDTO requestresponse.APIResponse){
+	var CoordinadorNuevo map[string]interface{}
+	var resultado map[string]interface{}
+	var status = 0
+
+	if err := json.Unmarshal(data, &CoordinadorNuevo); err == nil {
+		if resultado["Type"] != "error" {
+			var CoordinadorAntiguos []map[string]interface{}
+			idStr := fmt.Sprintf("%v", CoordinadorNuevo["ProyectoAcademicoInstitucionId"].(map[string]interface{})["Id"])
+
+			errcordinador := request.GetJson("http://"+beego.AppConfig.String("ProyectoAcademicoService")+"/proyecto_academico_rol_tercero_dependencia/?query=ProyectoAcademicoInstitucionId.Id:"+idStr, &CoordinadorAntiguos)
+			if errcordinador == nil {
+				if CoordinadorAntiguos[0]["Id"] != nil {
+					if exito := ManejoCoordinadorAntiguo(CoordinadorAntiguos); !exito {
+						status = 400
+					}
+					if exito := RegistrarCoordinador(CoordinadorNuevo); !exito {
+						status = 400
+					}
+
+					APIResponseDTO = requestresponse.APIResponseDTO(true, 200, nil)
+					return APIResponseDTO
+				} else {
+					if err := json.Unmarshal(data, &CoordinadorNuevo); err == nil {
+						if exito := RegistrarCoordinador(CoordinadorNuevo); !exito {
+							status = 400
+						} else {
+							status = 200
+						}
+					} else {
+						status = 400
+					}
+				}
+			} else {
+				APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil, errcordinador.Error())
+				return APIResponseDTO
+			}
+		} else {
+			if resultado["Body"] == "<QuerySeter> no row found" {
+				APIResponseDTO = requestresponse.APIResponseDTO(false, 404, nil)
+				return APIResponseDTO
+			} else {
+				APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil, resultado["Body"])
+				return APIResponseDTO
+			}
+		}
+	} else {
+		APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil, err.Error())
+		return APIResponseDTO
+	}
+	if status != 400{
+		APIResponseDTO = requestresponse.APIResponseDTO(true, 200, nil)
+	}else if status == 404{
+		APIResponseDTO = requestresponse.APIResponseDTO(false, 404, nil)
+	}else {
+		APIResponseDTO = requestresponse.APIResponseDTO(false, 400, nil)
+	}
+	return APIResponseDTO
 }
